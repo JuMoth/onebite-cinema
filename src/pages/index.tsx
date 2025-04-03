59191c9200262b453bc59155e5699b36a7c4e9f0
JuMoth <tkdlqjqkd@gmail.com>
2025-04-03 22:16:29 +0900

    Feat: Global Layout & Search-bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { ReactNode } from "react";
+import SearchableLayout from "@/components/searchable-layout";
+
 export default function Home() {
-  return (
-    <>
-      <h1>ONEBITE CINEMA</h1>
-    </>
-  );
+  return <h2>ONEBITE CINEMA</h2>;
+}
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
 }
